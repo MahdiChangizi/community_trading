d46af493b43092b71effe2862d241d2a3388e488
mahdi changizi <mchangizi10@gmail.com>
2025-09-29 08:00:37 +0200

    add logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>CryptoTrade Community - Premium Trading Platform</title>
+    <link rel="icon" type="image/x-icon" href="{{asset("images/Logo-bg.png")}}">
     <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
